e834fb169a051d34c7aa0d1ca42f6b9e43a33765
Samuel Wittlinger <samo.wittlinger@gmail.com>
2023-09-27 11:20:32 +0200

    feat: better, polished api

--- src/Result.ts
+++ src/Result.ts
@@ -1,3 +1,6 @@
+/**
+ * A type representing the result of a computation that may succeed or fail.
+ */
 export type Result<T, E = null> = Ok<T> | Err<E>
 
 function isPromise<T>(value: unknown): value is Promise<T> {
@@ -11,14 +14,37 @@ function isPromise<T>(value: unknown): value is Promise<T> {
 
 // eslint-disable-next-line @typescript-eslint/no-namespace, @typescript-eslint/no-redeclare
 export namespace Result {
+   /**
+    * Creates a new `Ok` variant.
+    *
+    * @typeparam T The type of the value contained in the `Ok`.
+    * @param data The value to contain in the `Ok`.
+    */
    export function ok<T>(data: T): Ok<T> {
       return new Ok(data)
    }
 
-   export function err<E>(data: E, error?: Error | string): Err<E> {
+   /**
+    * Creates a new `Err` variant.
+    *
+    * @typeparam E The type of the value contained in the `Err`.
+    * @param data The value to contain in the `Err`.
+    * @param error An optional error to contain in the `Err`.
+    */
+   export function err<E = null>(
+      data: E = null as E,
+      error?: Error | string,
+   ): Err<E> {
       return new Err(data, error)
    }
 
+   /**
+    * Creates a `Result` from a function, a promise, or a promise-returning function.
+    * If an error is thrown at any point, it is caught and wrapped in an `Err`.
+    * Otherwise, the result is wrapped in an `Ok`.
+    *
+    * @param fnOrThenable A function, a promise, or a promise-returning function.
+    */
    export function from<T>(fnOrThenable: Promise<T>): Promise<Result<T>>
    export function from<T>(fnOrPromise: () => Promise<T>): Promise<Result<T>>
    export function from<T, E>(fnOrPromise: () => T): Result<T>
@@ -49,7 +75,10 @@ export namespace Result {
                      resolve(new Ok(data))
                   })
                   .catch((err) => {
-                     resolve(new Err(err))
+                     if (err instanceof Error) {
+                        resolve(new Err(null, err))
+                     }
+                     resolve(new Err(null, new Error(JSON.stringify(err))))
                   })
             })
          }
@@ -62,34 +91,113 @@ export namespace Result {
       }
    }
 }
+
+// Note: some of the type signatures are a bit loose in order to get better type inference
+
+/**
+ * A class representing the result of a computation.
+ */
 export abstract class _Result<T, E> {
    public abstract readonly value: T | E
+
+   /**
+    * Returns `true` if the result is an `Ok` variant.
+    */
 
    public isOk(): this is Ok<T> {
       return this instanceof Ok
    }
 
+   /**
+    * Returns `true` if the result is an `Err` variant.
+    */
+
    public isErr(): this is Err<E> {
       return this instanceof Err
    }
 
-   public andThen<D>(fn: (data: T) => Result<D, E>): Result<D, E> {
-      if (this.isOk()) {
-         return fn(this.value)
-      } else if (this.isErr()) {
-         return this as unknown as Result<D, E>
-      }
-      throw new Error("Invalid state")
-   }
+   /**
+    * Returns the contained `Ok` value, consuming the `self` value.
+    *
+    * @throws If the value is an `Err`, throws an error alongisde its cause.
+    */
+
+   public abstract unwrap(): T
+
+   /**
+    * Returns  the contained `Ok` value or a provided default.
+    *
+    * @param defaultValue The default value to return if the result is an `Err`.
+    */
+
+   public abstract unwrapOr<U>(defaultValue: T): T | U
+
+   /**
+    * Returns the contained `Err` value, consuming the `self` value.
+    *
+    * @throws If the value is an `Ok`, throws an error.
+    */
+
+   public abstract unwrapErr(): E
+
+   /**
+    * Returns the contained `Ok` value, consuming the `self` value.
+    *
+    * @throws If the value is an `Err`, throws an error with the provided message.
+    */
 
    public abstract expect(message: string): T
+
+   /**
+    * Returns the contained `Err` value, consuming the `self` value.
+    *
+    * @throws If the value is an `Ok`, throws an error with the provided message.
+    */
+
    public abstract expectErr(message: string): E
-   public abstract unwrap(): T
-   public abstract unwrapOr(value: T): T
-   public abstract unwrapErr(): E
-   public abstract unwrapOrThrow(): T
+
+   /**
+    * Calls an appropriate function based on the result based on if it is an `Ok` or an `Err`.
+    *
+    * @param fn.ok The function to call if the result is an `Ok`.
+    * @param fn.err The function to call if the result is an `Err`.
+    */
+
+   public abstract match<D>(fn: {
+      ok: (value: T) => D
+      err: (value: E) => D
+   }): D
+
+   /**
+    * Calls op if the result is Ok, otherwise returns the Err value of self.
+    *
+    * @param fn The function to call if the result is an `Ok`.
+    */
+
+   public abstract andThen<D>(fn: (data: T) => Result<D>): unknown
+
+   /**
+    * Maps a `Result<T, E>` to `Result<U, E>` by applying a function to a contained `Ok` value, leaving an `Err` value untouched.
+    *
+    * @param fn The function to call if the result is an `Ok`.
+    */
+
+   public abstract map(fn: (data: T) => unknown): unknown
+
+   /**
+    * Maps a `Result<T, E>` to `Result<T, F>` by applying a function to a contained `Err` value, leaving an `Ok` value untouched.
+    *
+    * @param fn The function to call if the result is an `Err`.
+    */
+
+   public abstract mapErr(fn: (error: E) => unknown): unknown
 }
 
+/**
+ * A class representing a successful result of a computation.
+ *
+ * @typeparam T The type of the value contained in the `Ok`.
+ */
 export class Ok<T> extends _Result<T, never> {
    public readonly value: T
 
@@ -102,11 +210,7 @@ export class Ok<T> extends _Result<T, never> {
       return this.value
    }
 
-   public unwrapOr(value: T): T {
-      return this.value
-   }
-
-   public unwrapOrThrow(): T {
+   public unwrapOr(value: unknown): T {
       return this.value
    }
 
@@ -122,16 +226,33 @@ export class Ok<T> extends _Result<T, never> {
       throw new Error("Cannot unwrap an Ok as an Err")
    }
 
-   public match<D>(fn: { ok: (data: T) => D; err: unknown }): D {
+   public match<D>(fn: { ok: (value: T) => D; err: unknown }): D {
       return fn.ok(this.value)
+   }
+
+   public andThen<D>(fn: (data: T) => Result<D>): Result<D> {
+      return fn(this.value)
+   }
+
+   public map<D>(fn: (data: T) => D): Ok<D> {
+      return Result.ok(fn(this.value))
+   }
+
+   public mapErr<D>(fn: unknown): this {
+      return this
    }
 }
 
+/**
+ * A class representing a failed result of a computation.
+ *
+ * @typeparam E The type of the value contained in the `Err`.
+ */
 export class Err<E> extends _Result<never, E> {
    public readonly value: E
    public readonly error: Error
 
-   public constructor(value: E, error?: Error | string) {
+   public constructor(value: E = null as E, error?: Error | string) {
       super()
       this.value = value
       if (error instanceof Error) {
@@ -145,7 +266,7 @@ export class Err<E> extends _Result<never, E> {
       }
    }
 
-   public unwrapOr(value: never): never {
+   public unwrapOr<T>(value: T): T {
       return value
    }
 
@@ -157,10 +278,6 @@ export class Err<E> extends _Result<never, E> {
       throw new Error("Cannot unwrap an Err", { cause: this.error })
    }
 
-   public unwrapOrThrow(): never {
-      throw this.error
-   }
-
    public expect(message: string): never {
       throw new Error(message, { cause: this.error })
    }
@@ -169,27 +286,33 @@ export class Err<E> extends _Result<never, E> {
       return this.value
    }
 
-   public match<D>(fn: { ok: unknown; err: (error: E) => D }): D {
+   public match<D>(fn: { ok: unknown; err: (value: E) => D }): D {
       return fn.err(this.value)
+   }
+
+   public andThen(fn: unknown): this {
+      return this
+   }
+
+   public map<D>(fn: unknown): this {
+      return this
+   }
+
+   public mapErr<D>(fn: (error: E) => D): Err<D> {
+      return Result.err(fn(this.value))
    }
 }
 
-// function x(s: boolean) {
-//    if (s) {
-//       return Result.ok(1)
-//    }
-//    return Result.err("2", new Error("Error"))
-// }
+function x(s: boolean, b: boolean = true) {
+   if (s) {
+      return Result.ok(1)
+   }
 
-// function y() {
-//    const result = x(false)
+   return Result.err()
+}
 
-//    const c = result.match({
-//       err: (err) => err,
-//       ok: (data) => "s",
-//    })
+function y() {
+   const result = x(true).map((data) => "3")
+}
 
-//    console.log(c)
-// }
-
-// y()
+y()
